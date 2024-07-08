7d5493c6ef49b9c99934cae0a30f7ea7cc8769fe
GhostKato <osadchenko.maksym@gmail.com>
2024-07-08 05:11:14 +0300

    commit5

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,18 @@
 import s from './Contact.module.css';
+import { FaUser, FaPhone } from "react-icons/fa6";
 
 const Contact = ({name, number, textBtn, deleteContact, id}) => {
   return (
     <div className={s.container}>    
-   <div className={s.contact}>
-      <p className={s.name}>{name}</p>
-      <p className={s.number}>{number}</p>
+      <div className={s.contact}>
+        <div className={s.containerItem}>
+          <FaUser className={s.icon} />
+          <p className={s.name}>{name}</p>
+        </div>
+        <div className={s.containerItem}>
+          <FaPhone className={s.icon}/>
+        <p className={s.number}>{number}</p>
+        </div>
    </div>
       <button className={s.btn} onClick={() => deleteContact(id)}>{textBtn}</button>
     </div>
